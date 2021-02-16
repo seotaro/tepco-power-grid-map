1e7ef0b5eb3232b3a25cbdfcccb0c30e69a6ab36
Taro Seo <mail2017@seotaro.com>
2021-02-16 21:45:59 +0900

    Initial commit.

---
+++ app.js
@@ -0,0 +1,47 @@
+"use strict";
+
+(function () {
+  mapboxgl.accessToken =
+    "pk.eyJ1Ijoic2VvdGFybyIsImEiOiJjazA2ZjV2ODkzbmhnM2JwMGYycmc5OTVjIn0.5k-2FWYVmr5FH7E4Uk6V0g";
+
+  var map = new mapboxgl.Map({
+    container: "map",
+    style: "mapbox://styles/seotaro/ckccx2ir32ft21ilprmbedyud",
+    center: [139.5, 36.0],
+    zoom: 8,
+    antialias: true,
+  });
+
+  map.on("load", function () {
+    map.addSource('blackout', {
+      type: "geojson",
+      data: "https://tepco-power-grid-hack-mrfbzypr4q-an.a.run.app/v1/blackout.geojson",
+    });
+
+    map.addLayer({
+      id: "blackout-fill",
+      type: 'fill',
+      source: 'blackout',
+
+      layout: {},
+      paint: {
+        'fill-outline-color': '#111',
+        'fill-color': '#FFA500',
+        'fill-opacity': 0.75
+      }
+    });
+
+
+    // map.addLayer({
+    //   id: "blackout-line",
+    //   type: 'line',
+    //   source: 'blackout',
+
+    //   layout: {},
+    //   paint: {
+    //     'line-width': 3,
+    //     'line-color': '#000',
+    //   }
+    // });
+  });
+})();
