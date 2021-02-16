976e73eba8ef75e3420d19a00814739526970cc1
Taro Seo <mail2017@seotaro.com>
2021-02-16 21:57:56 +0900

    ライセンス情報を追加する。

--- app.js
+++ app.js
@@ -16,6 +16,8 @@
     map.addSource('blackout', {
       type: "geojson",
       data: "https://tepco-power-grid-hack-mrfbzypr4q-an.a.run.app/v1/blackout.geojson",
+      attribution:
+        '<a href="https://nlftp.mlit.go.jp/ksj/gml/datalist/KsjTmplt-N03-v2_3.html" target="_blank">国土交通省国土数値情報ダウンロードサイト</a>',
     });
 
     map.addLayer({
